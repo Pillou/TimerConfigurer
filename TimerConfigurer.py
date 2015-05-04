635c78c8f695a15ebacd019d88796585d339cdf5
Pillou <lecam.guillaume@gmail.com>
2015-05-05 00:24:44 +0200

    add header and advance on brute force algo

--- TimerConfigurer.py
+++ TimerConfigurer.py
@@ -1,3 +1,23 @@
+#  TimerConfigurer.py
+#
+#  Copyright 2015 Guillaume Le Cam
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
 import sys
 import json
 
@@ -30,14 +50,21 @@ class Timer :
         self.compare = 0
         self.prescaler = 0
 
+    def GetMinPrescaler(self):
+        min_prescaler = ComputePrescaler(self.clk_freq, self.wanted_freq, self.compare_range.get_max() )
+        min_prescaler = max(int(min_prescaler), self.prescaler_range.get_min())
+        return min_prescaler
+
+    def GetMaxPrescaler(self):
+        max_prescaler = ComputePrescaler(self.clk_freq, self.wanted_freq, self.compare_range.get_min() )
+        max_prescaler = min(int(max_prescaler+1), self.prescaler_range.get_max())
+        return max_prescaler
+
+
     def computeBestFreq(self):
-        min_prescaler = int(self.wanted_freq * self.compare_range.get_max() / self.clk_freq)
-        print("min_prescaler = {0}".format(min_prescaler))
-        if(min_prescaler<self.prescaler_range.get_min()):
-            print("ERROR: the prescaler range is not enough to meet the requirements")
-            return
-        current_prescaler = min_prescaler
-        while( current_prescaler <= self.prescaler_range.get_max()):
+        print("min_prescaler = {0}, max_prescaler = {1}".format(self.GetMinPrescaler(), self.GetMaxPrescaler()))
+        current_prescaler = self.GetMinPrescaler()
+        while( current_prescaler <= self.GetMaxPrescaler()):
             compare_value = max(int(round(self.clk_freq / (current_prescaler * self.wanted_freq))), 1)
             error = self.clk_freq/(current_prescaler*compare_value)-self.wanted_freq
             print("compare_value = {0}, error = {1}".format(compare_value, error))
@@ -45,17 +72,23 @@ class Timer :
             if( error == 0):
                 break
             current_prescaler = current_prescaler * 2
-        
-        
+
+
+
+def ComputePrescaler(ref_clk, wanted_freq, compare_value):
+    return (ref_clk / (wanted_freq * compare_value) )
+
+def ComputeCompareValue(ref_clk, wanted_freq, prescaler):
+    return (ref_clk / (wanted_freq * prescaler) )
 
 
 if __name__ == "__main__":
     print("Starting TimerConfigurer...")
     print(sys.argv)
-    with open('test.json') as data_file:    
+    with open('test.json') as data_file:
         data = json.load(data_file)
     print(data)
-    
+
     assert(data["clock"])
     assert(data["prescaler_range"])
     assert(data["compare_range"])
@@ -67,4 +100,4 @@ if __name__ == "__main__":
     timer.computeBestFreq()
 
 
-    
+
